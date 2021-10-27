4b3839869decfc6f3da904ff5f7805fe5fa7c3c0
Lalit Rana <lalit.rana@nagarro.com>
2021-10-27 16:08:28 +0530

    router created

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const Post = require('./models/post');
+const postRoutes = require('./routes/posts');
 
 const app = express();
 const cors = require('cors');
@@ -40,55 +40,6 @@ app.use((req, res, next) => {
 	next();
 });
 
-app.post('/api/posts', (req, res, next) => {
-	const post = new Post({
-		title: req.body.title,
-		content: req.body.content,
-	});
-	post.save();
-	console.log(post);
-	res.status(201).json({
-		message: 'Post added successfully',
-	});
-});
-
-app.put('/api/posts/:_id', (req, res, next) => {
-	const post = new Post({
-		_id: req.params._id,
-		title: req.body.title,
-		content: req.body.content,
-	});
-	Post.updateOne({ _id: req.params._id }, post).then((result) => {
-		console.log(result);
-		res.status(200).json({ message: 'Update successfull' });
-	});
-});
-
-app.get('/api/posts', (req, res, next) => {
-	Post.find().then((documents) => {
-		res.status(200).json({
-			message: 'Posts fetched successfully!',
-			posts: documents,
-		});
-		console.log(documents);
-	});
-});
-
-app.get('/api/posts/:_id', (req, res, next) => {
-	Post.findById(req.params._id).then((post) => {
-		if (post) {
-			res.status(200).json(post);
-		} else {
-			res.status(404).json({ message: 'Post not found' });
-		}
-	});
-});
-
-app.delete('/api/posts/:_id', (req, res, next) => {
-	Post.deleteOne({ _id: req.params._id }).then((result) => {
-		console.log(result);
-		res.status(200).json({ message: 'post deleted' });
-	});
-});
+app.use('/api/posts',postRoutes);
 
 module.exports = app;
