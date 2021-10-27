576fa2d53c9607e7bfce9640435541ba5bec11c3
Lalit Rana <lalit.rana@nagarro.com>
2021-10-27 16:59:05 +0530

    backend error handeling finished

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,9 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const postRoutes = require('./routes/posts');
 
 const app = express();
-const cors = require('cors');
 
 mongoose
 	.connect(
@@ -18,14 +16,7 @@ mongoose
 		console.log('Connection failed');
 	});
 
-const corsOptions = {
-	origin: 'http://localhost:4200',
-	credentials: true, //access-control-allow-credentials:true
-	optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
-
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.use((req, res, next) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');
@@ -40,6 +31,6 @@ app.use((req, res, next) => {
 	next();
 });
 
-app.use('/api/posts',postRoutes);
+app.use('/api/posts', postRoutes);
 
 module.exports = app;
--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -8,11 +8,14 @@ router.post('', (req, res, next) => {
 		title: req.body.title,
 		content: req.body.content,
 	});
-	post.save();
-	console.log(post);
-	res.status(201).json({
-		message: 'Post added successfully',
-	});
+	post
+		.save()
+		.then((result) => {
+			res.status(201).json({ message: 'Post added successfully' });
+		})
+		.catch((error) => {
+			res.status(500).json({ message: 'Internal Server Error' });
+		});
 });
 
 router.put('/:_id', (req, res, next) => {
@@ -21,37 +24,47 @@ router.put('/:_id', (req, res, next) => {
 		title: req.body.title,
 		content: req.body.content,
 	});
-	Post.updateOne({ _id: req.params._id }, post).then((result) => {
-		console.log(result);
-		res.status(200).json({ message: 'Update successfull' });
-	});
+	Post.updateOne({ _id: req.params._id }, post)
+		.then((result) => {
+			res.status(200).json({ message: 'Update successfull' });
+		})
+		.catch((error) => {
+			res.status(500).json({ message: 'Internal Server Error' });
+		});
 });
 
 router.get('', (req, res, next) => {
-	Post.find().then((documents) => {
-		res.status(200).json({
-			message: 'Posts fetched successfully!',
-			posts: documents,
+	Post.find()
+		.then((documents) => {
+			res.status(200).json({
+				message: 'Posts fetched successfully!',
+				posts: documents,
+			});
+		})
+		.catch((error) => {
+			res.status(500).json({ message: 'Internal Server Error' });
 		});
-		console.log(documents);
-	});
 });
 
 router.get('/:_id', (req, res, next) => {
-	Post.findById(req.params._id).then((post) => {
-		if (post) {
+	Post.findById(req.params._id)
+		.then((post) => {
 			res.status(200).json(post);
-		} else {
+		})
+		.catch((error) => {
 			res.status(404).json({ message: 'Post not found' });
-		}
-	});
+		});
 });
 
 router.delete('/:_id', (req, res, next) => {
-	Post.deleteOne({ _id: req.params._id }).then((result) => {
-		console.log(result);
-		res.status(200).json({ message: 'post deleted' });
-	});
+	Post.deleteOne({ _id: req.params._id })
+		.then((result) => {
+			console.log(result);
+			res.status(200).json({ message: 'post deleted' });
+		})
+		.catch((error) => {
+			res.status(500).json({ message: 'Internal Server Error' });
+		});
 });
 
 module.exports = router;
